ccddf965fa5b080da5935a4d064d2fe12042bfe9
Yoshinori Hirose <44sheetsofseaweed@gmail.com>
2017-02-27 17:17:28 +0900

    "Statically" Including a Fragment in an Activity Layout

---
+++ app/src/main/java/com/gmail/yoshinorihirosetraining/androidfragmentstutorial/FirstFragment.java
@@ -0,0 +1,26 @@
+package com.gmail.yoshinorihirosetraining.androidfragmentstutorial;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FirstFragment extends Fragment implements View.OnClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                              ViewGroup container,
+                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.first_fragment, container, false);
+        Button nextButton = (Button)view.findViewById(R.id.button_first);
+        nextButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        ;
+    }
+}
---
+++ app/src/main/java/com/gmail/yoshinorihirosetraining/androidfragmentstutorial/MainActivity.java
@@ -0,0 +1,25 @@
+package com.gmail.yoshinorihirosetraining.androidfragmentstutorial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_statically_including) public void onClick1(View view) {
+        startActivity(new Intent(this, FirstStaticallyEmbeddingActivity.class)); }
+}
