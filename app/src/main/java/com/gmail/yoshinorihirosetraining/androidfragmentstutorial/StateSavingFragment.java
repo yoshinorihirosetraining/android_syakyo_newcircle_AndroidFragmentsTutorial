161e1f67ed5683fd4b72f15e48184a16758116c8
Yoshinori Hirose <44sheetsofseaweed@gmail.com>
2017-02-27 22:07:07 +0900

    Saving Fragment State

---
+++ app/src/main/java/com/gmail/yoshinorihirosetraining/androidfragmentstutorial/StateSavingFragment.java
@@ -0,0 +1,28 @@
+package com.gmail.yoshinorihirosetraining.androidfragmentstutorial;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class StateSavingFragment extends Fragment implements View.OnClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                              ViewGroup container,
+                              Bundle savedInstanceState) {
+        Log.d(getClass().getSimpleName(), "onCreate()");
+        View view = inflater.inflate(R.layout.first_fragment, container, false);
+        Button nextButton = (Button)view.findViewById(R.id.button_first);
+        nextButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        ;
+    }
+}
